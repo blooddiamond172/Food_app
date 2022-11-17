c4384e7c26107cfd9be35d1ea4180981be1b503c
blooddiamond172 <dungnv1722001@gmail.com>
2022-11-17 15:29:35 +0700

    update module cart

--- FoodApp/src/main/java/com/dungnv/dao/CartDAO.java
+++ FoodApp/src/main/java/com/dungnv/dao/CartDAO.java
@@ -15,23 +15,27 @@ public class CartDAO {
 	private static String URL = "jdbc:mysql://localhost:3306/food_app";
 	private static String username = "root";
 	private static String password = "nguyenvandung";
+	Connection con = getConnection();
 	
 	private String SELECT_USER_CART = 
-			"SELECT * FROM user_cart \r\n"
+			"SELECT cart_id FROM user_cart \r\n"
 			+ "WHERE user_id = ? \r\n"
 			+ "order by cart_id DESC "
 			+ "LIMIT 1;";
-	private String SELECT_PRODUCT_IN_CART = 
+	private String SELECT_CART = 
 			"SELECT p.product_id, p.name, p.price, p.image_link, p.short_description, c.quantity, c.new_price\r\n"
 			+ "FROM product as p join cart as c USING (product_id)\r\n"
 			+ "WHERE cart_id = ?;";
-	private String SELECT_PRODUCT_ID = 
-			"SELECT * FROM food_app.cart "
+	private String SELECT_PRODUCT_ID =
+			"SELECT * FROM food_app.cart\r\n"
 			+ "WHERE cart_id = ? and product_id = ?;";
 	private String SELECT_TOTAL = 
 			"SELECT sum(new_price) as \"total\" FROM food_app.cart\r\n"
 			+ "WHERE cart_id = ?;";
 	
+	private String SELECT_QUANTITY_OF_PRODUCT = 
+			"SELECT quantity FROM food_app.cart "
+			+ "WHERE cart_id = ? and product_id = ?";
 	private String INSERT_CART = 
 			"INSERT INTO food_app.user_cart(user_id) VALUE(?);";
 	private String INSERT_PRODUCT_TO_CART =
@@ -39,7 +43,7 @@ public class CartDAO {
 	private String DELETE_PRODUCT_IN_CART =
 			"DELETE FROM food_app.cart "
 			+ "WHERE cart_id=? and product_id=? ;";
-	private String UPDATE_PRODUCT_IN_CART =
+	private String UPDATE_CART =
 			"UPDATE cart SET quantity=?, new_price=? "
 			+ "WHERE cart_id = ? and product_id = ?;";
 	
@@ -54,10 +58,11 @@ public class CartDAO {
 		return null;
 	}
 
-	public int createCart(Integer id) {
-		Connection con = getConnection();
+	public int createCartID(Integer id) {
+		PreparedStatement preparedStatement;
+		
 		try {
-			PreparedStatement preparedStatement = con.prepareStatement(INSERT_CART);
+			preparedStatement = con.prepareStatement(INSERT_CART);
 			preparedStatement.setInt(1, id);
 			return preparedStatement.executeUpdate();
 		} catch (SQLException e) {
@@ -66,64 +71,61 @@ public class CartDAO {
 		return 0;
 	}
 
-	public UserCart getUserCart(Integer id) {
-		Connection con = getConnection();
+	public Integer getCartID(Integer id) {
 		try {
+			ResultSet resultSet;
 			PreparedStatement preparedStatement = con.prepareStatement(SELECT_USER_CART);
 			preparedStatement.setInt(1, id);
-			ResultSet resultSet = preparedStatement.executeQuery();
+			resultSet = preparedStatement.executeQuery();
 			while (resultSet.next()) {
-				Integer cartID = resultSet.getInt("cart_id");
-				Integer userID = resultSet.getInt("user_id");
-				UserCart uc = new UserCart(cartID,userID);
-				return uc;
+				return resultSet.getInt("cart_id");
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+		
 		return null;
 	}
 
-	public int insertProductToCart(Integer cartID, String productID,
-			Integer price) {
-		Connection con = getConnection();
+	public int addProductToCart(Integer cartID, Product product) {
 		try {
 			PreparedStatement preparedStatement = con.prepareStatement(INSERT_PRODUCT_TO_CART);
 			preparedStatement.setInt(1, cartID);
-			preparedStatement.setString(2, productID);
+			preparedStatement.setString(2, product.getProductID());
 			preparedStatement.setInt(3, 1);
-			preparedStatement.setInt(4, price);
+			preparedStatement.setInt(4, product.getPrice());
 			System.out.println(preparedStatement);
 			return preparedStatement.executeUpdate();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+		
 		return 0;
 	}
 
 	public ArrayList<Cart> getCart(Integer cartID) {
-		Connection con = getConnection();
 		ArrayList<Cart> carts = new ArrayList<>();
+						
 		try {
-			PreparedStatement preparedStatement = con.prepareStatement(SELECT_PRODUCT_IN_CART);
+			ResultSet resultSet;
+			PreparedStatement preparedStatement = con.prepareStatement(SELECT_CART);
 			preparedStatement.setInt(1, cartID);
-			System.out.println(preparedStatement);
-			ResultSet resultSet = preparedStatement.executeQuery();
+			resultSet = preparedStatement.executeQuery();
 			while (resultSet.next()) {
 				String productID = resultSet.getString("product_id");
 				String name = resultSet.getString("name");
-				Integer price = resultSet.getInt("price");
 				String imageLink = resultSet.getString("image_link");
+				Integer price = resultSet.getInt("price");
 				String shortDescription = resultSet.getString("short_description");
-				Product product = new Product(productID,name,price,imageLink,shortDescription);
+				Product product = new Product(productID, name, price, imageLink, shortDescription); 
 				Integer quantity = resultSet.getInt("quantity");
-				Integer new_price = resultSet.getInt("new_price");
-				Cart cart = new Cart(cartID,product,quantity,new_price);
-				carts.add(cart);				
+				Integer new_price = resultSet.getInt("new_price");				
+				carts.add(new Cart(cartID, product, quantity, new_price));
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+		
 		return carts;
 		
 	}
@@ -143,56 +145,87 @@ public class CartDAO {
 		return 0;
 	}
 
-	public Cart getProductInCart(Product product, Integer cartID) {
-		Connection con = getConnection();
-		try {
-			PreparedStatement preparedStatement = con.prepareStatement(SELECT_PRODUCT_ID);
-			preparedStatement.setInt(1, cartID);
-			preparedStatement.setString(2, product.getProductID());
-			System.out.println(preparedStatement);
-			ResultSet resultSet = preparedStatement.executeQuery();
-			while (resultSet.next()) {
-				String productID = resultSet.getString("product_id");
-				Integer quantity = resultSet.getInt("quantity");
-				Integer price = resultSet.getInt("new_price");
-				Cart cart = new Cart(cartID,product,quantity,price);
-				return cart;
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return null;
-		
-	}
+//	public Cart getProductsInCart(Integer cartID) {
+//		try {
+//			PreparedStatement preparedStatement = con.prepareStatement(SELECT_PRODUCT_ID);
+//			preparedStatement.setInt(1, cartID);
+//			preparedStatement.setString(2, productID);
+//			System.out.println(preparedStatement);
+//			ResultSet resultSet = preparedStatement.executeQuery();
+//			while (resultSet.next()) {
+//				String idOfProduct = resultSet.getString("product_id");
+//				Integer quantity = resultSet.getInt("quantity");
+//				Integer price = resultSet.getInt("new_price");
+//				Cart cart = new Cart(cartID,idOfProduct,quantity,price);
+//				return cart;
+//			}
+//			
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//		}
+//		return null;
+//		
+//	}
 
-	public int updateProductToCart(Cart cart) {
-		Connection con = getConnection();
+	public int updateCart(Integer cartID, Product product) {
 		try {
-			PreparedStatement preparedStatement = con.prepareStatement(UPDATE_PRODUCT_IN_CART);
-			preparedStatement.setInt(1, cart.getQuantity()+1);
-			preparedStatement.setInt(2, (cart.getQuantity()+1)*(cart.getProduct().getPrice()));
-			preparedStatement.setInt(3, cart.getCartID());
-			preparedStatement.setString(4, cart.getProduct().getProductID());
-			return preparedStatement.executeUpdate();
+			ResultSet resultSet;
+			Integer quantity = null;
+			PreparedStatement preparedStatement1 = con.prepareStatement(SELECT_QUANTITY_OF_PRODUCT);
+			PreparedStatement preparedStatement2 = con.prepareStatement(UPDATE_CART);
+			preparedStatement1.setInt(1, cartID);
+			preparedStatement1.setString(2, product.getProductID());
+			resultSet = preparedStatement1.executeQuery();
+			while (resultSet.next()) {
+				quantity = resultSet.getInt("quantity");
+			}
+			preparedStatement2.setInt(1, (quantity + 1));
+			preparedStatement2.setInt(2, ((quantity + 1) * product.getPrice()));
+			preparedStatement2.setInt(3, cartID);
+			preparedStatement2.setString(4, product.getProductID());
+			return preparedStatement2.executeUpdate();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+		
 		return 0;
 	}
 
 	public Integer getToTal(Integer cartID) {
-		Connection con = getConnection();
 		try {
+			ResultSet resultSet;
 			PreparedStatement preparedStatement = con.prepareStatement(SELECT_TOTAL);
 			preparedStatement.setInt(1, cartID);
-			ResultSet resultSet = preparedStatement.executeQuery();
+			resultSet = preparedStatement.executeQuery();
 			while (resultSet.next()) {
-				Integer total = resultSet.getInt("total");
-				return total;
+				return (resultSet.getInt("total"));
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+		
+		return null;
+	}
+
+	public int isExist(Integer cartID, String productID) {
+		
+		try {
+			PreparedStatement preparedStatement = con.prepareStatement(SELECT_PRODUCT_ID);
+			
+			ResultSet resultSet;
+			
+			preparedStatement.setInt(1, cartID);
+			preparedStatement.setString(2, productID);
+			
+			resultSet = preparedStatement.executeQuery();
+			while (resultSet.next()) {
+				return 1;
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
 		return 0;
 	}
 }
